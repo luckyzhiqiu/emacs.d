e27b3ca4520e9f36f908d679172481aa49a2cce1
zhiqiu.liu <zhiqiu.liu@shopee.com>
2020-03-28 12:28:12 +0800

    mofify meta key binding

--- lisp/init-osx-keys.el
+++ lisp/init-osx-keys.el
@@ -3,8 +3,9 @@
 ;;; Code:
 
 (when *is-a-mac*
-  (setq mac-command-modifier 'meta)
-  (setq mac-option-modifier 'none)
+  ;;(setq mac-command-modifier 'meta)
+  ;;(setq mac-option-modifier 'none)
+  (setq mac-option-modifier 'meta)
   ;; Make mouse wheel / trackpad scrolling less jerky
   (setq mouse-wheel-scroll-amount '(1
                                     ((shift) . 5)
@@ -12,9 +13,9 @@
   (dolist (multiple '("" "double-" "triple-"))
     (dolist (direction '("right" "left"))
       (global-set-key (read-kbd-macro (concat "<" multiple "wheel-" direction ">")) 'ignore)))
-  (global-set-key (kbd "M-`") 'ns-next-frame)
-  (global-set-key (kbd "M-h") 'ns-do-hide-emacs)
-  (global-set-key (kbd "M-˙") 'ns-do-hide-others)
+  (global-set-key (kbd "M-`") 'ns-next-frame) ;; 跳转到另外一个frame
+  ;; (global-set-key (kbd "M-h") 'ns-do-hide-emacs) ;; 隐藏emacs
+  ;; (global-set-key (kbd "M-˙") 'ns-do-hide-others) ;;
   (after-load 'nxml-mode
     (define-key nxml-mode-map (kbd "M-h") nil))
   (global-set-key (kbd "M-ˍ") 'ns-do-hide-others) ;; what describe-key reports for cmd-option-h
